3c63cf435359ee7bc5f1bac1c00c90bb378d3dca
Ntnick-22 <nyeinthunaing322@gmail.com>
2026-05-24 15:07:36 +0100

    add NSG rules to aks subnet and add gitignore

--- 04_aks/main.tf
+++ 04_aks/main.tf
@@ -1,6 +1,53 @@
 # Dedicated subnet for AKS nodes.
 # Lives in the existing VNet from 01_networking.
 # /24 gives 251 usable IPs — enough for node IPs with kubenet.
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  location            = var.location
+  resource_group_name = data.terraform_remote_state.rg.outputs.resource_group_name
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-https"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-azure-lb-health-probe"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "AzureLoadBalancer"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "aks_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.aks_nsg.id
+}
+
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
   resource_group_name  = data.terraform_remote_state.rg.outputs.resource_group_name
